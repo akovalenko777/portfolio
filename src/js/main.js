23942e152aa4d95cc484275de2e7ed8fb76a98fb
akovalenko777 <akovalenko777@gmail.com>
2026-09-05 19:00:22 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+import PhotoSwipeLightbox from './photoswipe/photoswipe-lightbox.esm.min.js';
+import PhotoSwipe from './photoswipe/photoswipe.esm.min.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#project-gallery',
+  children: 'a',
+  pswpModule: () => PhotoSwipe
+});
+
+
+const el = (id) => document.getElementById(id)
+
+const modal = el('project-dialog')
+const backdrop = el('dialog-backdrop')
+document.addEventListener('click', (e) => {
+  if(e.target.classList.contains('detail-info-link')){
+    el('dialog-project-title').innerText = e.target.dataset.title
+    const template = el(e.target.dataset.template)
+    const clone = document.importNode(template.content, true)
+    el('project-modal-content').appendChild(clone)
+    lightbox.init();
+    modal.classList.add('open')
+    modal.setAttribute('aria-modal', 'true')
+    backdrop.classList.add('show')
+  }
+})
+
+const closeModal = () => {
+  modal.classList.remove('open')
+  modal.setAttribute('aria-modal', 'false')
+  backdrop.classList.remove('show')
+  el('project-modal-content').innerHTML = ''
+}
+
+el('close-dialog').addEventListener('click', () => {
+  closeModal()
+})
+
+document.addEventListener('keydown', (e) => {
+  if(e.code === 'Escape' && modal.classList.contains('open')) closeModal()
+})
