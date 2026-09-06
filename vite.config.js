894b0a305f8e4623e7b37e979c86cd482cbdb896
akovalenko777 <akovalenko777@gmail.com>
2026-09-06 23:45:55 +0300

    Fixed config, moved images into public dir

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,22 @@ import { readdirSync, statSync } from 'fs';
 import injectHTML from 'vite-plugin-html-inject';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// Helper function to recursively find all HTML files (excluding node_modules and dist)
 function getHtmlEntries(dir, entries = {}) {
   const files = readdirSync(dir);
-
   for (const file of files) {
     const fullPath = resolve(dir, file);
-
-    // Skip system or build directories
     if (file === 'node_modules' || file === 'dist' || file === 'src') continue;
 
     if (statSync(fullPath).isDirectory()) {
       getHtmlEntries(fullPath, entries);
     } else if (extname(file) === '.html') {
-      // Create a unique key based on the relative path (e.g., "about" or "blog/post-1")
       const relativePath = relative(process.cwd(), fullPath);
       const name = relativePath.replace(/\.html$/, '').replace(/\\/g, '/');
-
       entries[name || 'main'] = fullPath;
     }
   }
   return entries;
 }
-
-let resolvedBase = '/';
-let isBuild = false;
 
 export default defineConfig({
   base: '/portfolio/',
@@ -36,30 +27,14 @@ export default defineConfig({
     injectHTML(),
     ViteImageOptimizer({
       test: /\.(jpe?g|png|webp)$/i,
+      includePublic: true,
       jpeg: { quality: 80 },
       png: { quality: 80 },
       webp: {
         lossless: false,
         quality: 80,
       },
-    }),
-    {
-      name: 'replace-html-image-links',
-      configResolved(config) {
-        resolvedBase = config.base
-        isBuild = config.command === 'build'
-      },
-      transformIndexHtml: {
-        order: 'post',
-        handler(html) {
-          if (!isBuild) return html
-          return html.replace(
-            /<a href=".\/src\//g, 
-              '<a href="'+resolvedBase
-          );
-        }
-      }
-    }
+    })
   ],
 
   build: {
