253eb8893097be2d8859c0a475e8e0eb78425b15
akovalenko777 <akovalenko777@gmail.com>
2026-09-09 22:54:17 +0300

    moved 3d card script in main file

--- src/js/main.js
+++ src/js/main.js
@@ -64,3 +64,27 @@ el('show-all-projects').addEventListener('click', (e) => {
   e.target.parentElement.remove()
   el('all-projects').classList.add('show')
 })
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	const cards = document.querySelectorAll(".skill-panel")
+	const handleMouseMove = (e, card) => {
+		const rect = card.getBoundingClientRect()
+		const x = e.clientX - rect.left
+		const y = e.clientY - rect.top
+		const centerX = rect.width / 2
+		const centerY = rect.height / 2
+		const rotateX = ((y - centerY) / centerY) * -15
+		const rotateY = ((x - centerX) / centerX) * 15
+		card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(50px)`
+	}
+
+	const handleMouseLeave = (card) => {
+		card.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg) translateZ(0)`
+	}
+
+	cards.forEach((card) => {
+		card.addEventListener("mousemove", (e) => handleMouseMove(e, card))
+		card.addEventListener("mouseleave", () => handleMouseLeave(card))
+	})
+})
